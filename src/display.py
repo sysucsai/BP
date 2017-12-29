806dd8c2da6491e62c54ef254b188625594d602b
pengbd <961862555@qq.com>
2017-12-29 19:46:20 +0800

    添加了按钮连接的槽函数
    但是还没有绘图和保存位图的操作，也没有调用算法

--- src/display.py
+++ src/display.py
@@ -53,6 +53,9 @@ class DisplayWindow(QWidget):
         super(DisplayWindow,self).__init__(parent)
         self.resize(940,630)
 
+        self.train_number = -1
+        self.result_number = -1
+
         mainLayout = QHBoxLayout(self)
         canvasLayout = QHBoxLayout()
         menuLayout = QGridLayout()
@@ -70,25 +73,98 @@ class DisplayWindow(QWidget):
             button = QPushButton(str(i), self)
             self.button_number.append(button)
             self.button_number[i].resize(self.button_number[i].sizeHint())
-            menuLayout.addWidget(self.button_number[i], i/3, i%3)
+            #self.button_number[i].clicked.connect(self.choose_num)
+            if i > 0:
+                menuLayout.addWidget(self.button_number[i], (9-i)/3, (i+2)%3)
+            else:
+                menuLayout.addWidget(self.button_number[i], (9-i)/3, i%3)
+
+        self.button_number[0].clicked.connect(self.choose_num_0)
+        self.button_number[1].clicked.connect(self.choose_num_1)
+        self.button_number[2].clicked.connect(self.choose_num_2)
+        self.button_number[3].clicked.connect(self.choose_num_3)
+        self.button_number[4].clicked.connect(self.choose_num_4)
+        self.button_number[5].clicked.connect(self.choose_num_5)
+        self.button_number[6].clicked.connect(self.choose_num_6)
+        self.button_number[7].clicked.connect(self.choose_num_7)
+        self.button_number[8].clicked.connect(self.choose_num_8)
+        self.button_number[9].clicked.connect(self.choose_num_9)
 
         self.button_train = QPushButton("train", self)
-        menuLayout.addWidget(self.button_train, 3, 1, 1, 2)
+        self.button_train.clicked.connect(self.BP_train)
+        menuLayout.addWidget(self.button_train, 3, 1, 1, 1)
+
+        self.train_number_label = QLabel(self)
+        self.train_number_label.setText("-1")
+        self.train_number_label.setAlignment(Qt.AlignCenter)
+        self.train_number_label.setStyleSheet('background-color:#fff')
+        menuLayout.addWidget(self.train_number_label, 3, 2, 1, 1)
 
         self.button_iden = QPushButton("Identify", self)
+        self.button_iden.clicked.connect(self.BP_iden)
         menuLayout.addWidget(self.button_iden, 4, 0, 1, 2)
 
-        self.result = QLabel(self)
-        '''self.result.setFixedWidth(300)
-        self.result.setFixedHeight(300)'''
-        self.result.setText("9")
-        self.result.setAlignment(Qt.AlignCenter)
-        self.result.setStyleSheet('background-color:#FFF')
+        self.result_number_label = QLabel(self)
+        '''self.result_number_label.setFixedWidth(300)
+        self.result_number_label.setFixedHeight(300)'''
+        self.result_number_label.setText("-1")
+        self.result_number_label.setAlignment(Qt.AlignCenter)
+        self.result_number_label.setStyleSheet('background-color:#fff')
 
-        menuLayout.addWidget(self.result, 4, 2, 1, 1)
+        menuLayout.addWidget(self.result_number_label, 4, 2, 1, 1)
 
         mainLayout.addLayout(canvasLayout, 9)
         mainLayout.addLayout(menuLayout, 1)
+
+    def choose_num_0(self):
+        self.train_number = 0
+        self.train_number_label.setText(str(self.train_number))
+
+    def choose_num_1(self):
+        self.train_number = 1
+        self.train_number_label.setText(str(self.train_number))
+
+    def choose_num_2(self):
+        self.train_number = 2
+        self.train_number_label.setText(str(self.train_number))
+
+    def choose_num_3(self):
+        self.train_number = 3
+        self.train_number_label.setText(str(self.train_number))
+
+    def choose_num_4(self):
+        self.train_number = 4
+        self.train_number_label.setText(str(self.train_number))
+
+    def choose_num_5(self):
+        self.train_number = 5
+        self.train_number_label.setText(str(self.train_number))
+
+    def choose_num_6(self):
+        self.train_number = 6
+        self.train_number_label.setText(str(self.train_number))
+
+    def choose_num_7(self):
+        self.train_number = 7
+        self.train_number_label.setText(str(self.train_number))
+
+    def choose_num_8(self):
+        self.train_number = 8
+        self.train_number_label.setText(str(self.train_number))
+    
+    def choose_num_9(self):
+        self.train_number = 9
+        self.train_number_label.setText(str(self.train_number))
+
+    def BP_train(self):
+        if self.train_number >= 0:
+            #pass
+            self.train_number = -1
+            self.train_number_label.setText(str(self.train_number))
+        
+    def BP_iden(self):
+        self.result_number = -1
+        self.result_number_label.setText(str(self.result_number))
 
 if __name__ == '__main__' :
     app = QApplication(sys.argv)
